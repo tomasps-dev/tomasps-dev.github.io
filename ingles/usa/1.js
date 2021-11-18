acee17b543a966a3208ab03eae2aff8091a3fc28
Tomás Palma <79251435+tomasps-dev@users.noreply.github.com>
2021-11-18 10:22:40 +0100

    Create 1.js

---
+++ ingles/usa/1.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".secretpopout").delay(10000).fadeIn();
+});
+
+$(function() {
+  $(".stop").delay(15000).fadeIn();
+});
+
+$(function() {
+  $(".stopbig").delay(20000).fadeIn();
+});
